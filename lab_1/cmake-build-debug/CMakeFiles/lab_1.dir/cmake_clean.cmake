4c80b48cc22e41e8ee6b9eb5f27fc732bcbbd4ec
sideshowbobgot <sideshowbobgot@gmail.com>
2023-09-06 20:45:46 +0300

    removed .idea

---
+++ lab_1/cmake-build-debug/CMakeFiles/lab_1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_1.dir/TLibrary.cpp.o"
+  "CMakeFiles/lab_1.dir/TLibrary.cpp.o.d"
+  "CMakeFiles/lab_1.dir/TUser.cpp.o"
+  "CMakeFiles/lab_1.dir/TUser.cpp.o.d"
+  "CMakeFiles/lab_1.dir/TWorker.cpp.o"
+  "CMakeFiles/lab_1.dir/TWorker.cpp.o.d"
+  "CMakeFiles/lab_1.dir/main.cpp.o"
+  "CMakeFiles/lab_1.dir/main.cpp.o.d"
+  "lab_1"
+  "lab_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
